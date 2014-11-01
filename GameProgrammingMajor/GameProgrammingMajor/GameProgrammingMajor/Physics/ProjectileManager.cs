520fe2773e386c2a19a723c36e91e306cb99fb25
Deinyon Davies <m@ddoodm.com>
2014-11-01 15:54:16 +1100

    Oops, fixed projectile damage to 1/10 enemy health.

--- GameProgrammingMajor/GameProgrammingMajor/GameProgrammingMajor/Physics/ProjectileManager.cs
+++ GameProgrammingMajor/GameProgrammingMajor/GameProgrammingMajor/Physics/ProjectileManager.cs
@@ -108,7 +108,7 @@ namespace GameProgrammingMajor
             public float speed;
             public float screenTime = 0;
             public float creationTime = 0;
-            public float damage = 1.1f;
+            public float damage = 1f / 10f;
 
             public Projectile(ProjectileManager manager, StaticModel projectileModel, Vector3 start, Vector3 direction, float speed)
             {
